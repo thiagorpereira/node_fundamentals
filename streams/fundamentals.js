279bd59ce06df0be32b5b7f6662bcbc88cc5bdc2
Thiago Pereira <thiagorpereira7@gmail.com>
2023-09-14 17:53:16 -0300

    add: streams concepts

---
+++ streams/fundamentals.js
@@ -0,0 +1,38 @@
+// process.stdin
+//     .pipe(process.stdout)
+import { Readable, Writable, Transform } from 'node:stream'
+
+class OneToGundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i>100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+        this.push(buf) //chunk
+      }
+    }, 1000)
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToGundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
